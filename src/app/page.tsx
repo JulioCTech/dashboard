eec4f9b854498f89789bb7a59faa59f45ff596e1
JulioCTech <julinhubbs@gmail.com>
2025-07-04 04:08:25 -0300

    chore: adicionar shadcn/ui ao projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-  
 
+export default function Home() {
 
-  export default function Home () {
-
-    return (
-      <main>
-        <h1 className="text-4xl">Olá Mundo</h1>
-        <p>Vamos começar um novo projeto</p>
-        <span>Meu primeiro DASHBOARD</span>
-      </main>
-    )
-  }
+  return (
+    <main>
+      <h1 className="text-4xl">Olá Mundo</h1>
+      <p>Vamos começar um novo projeto</p>
+      <span>Meu primeiro DASHBOARD</span>
+    </main>
+  )
+}
